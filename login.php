d0831a127b53ca5c3e491b6f37948adb6da3ceea
Viktorija Barisic <vbarisic511@gmail.com>
2022-12-29 19:11:19 +0100

    izmjena login strane

--- login.php
+++ login.php
@@ -46,7 +46,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/login.css" />
-    <title>Biblioteka</title>
+    <title>Knjižara</title>
 </head>
 <body>
 
